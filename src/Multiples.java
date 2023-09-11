82e4eb3e7f4e449131d42e8325acccaba94d72a1
jacquelyn <jacquelyn.lu@mail.utoronto.ca>
2023-09-11 16:07:37 -0400

    new multiples.java

---
+++ src/Multiples.java
@@ -0,0 +1,19 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int n = 0;
+        for (int i = 1; i < 1001; i++){
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 && divisibleBy5) {
+                n++;
+            } else if (divisibleBy5){
+                n++;
+            } else if (divisibleBy3){
+                n++;
+            }
+
+        }
+        System.out.println(n);
+    }
+}
